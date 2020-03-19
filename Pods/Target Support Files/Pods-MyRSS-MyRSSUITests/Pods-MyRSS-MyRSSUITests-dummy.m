d3aaf4abf3acfa8b0dc2584ff07de030344cf403
Jaehooooooon <jaehoon572@naver.com>
2020-03-19 20:13:28 +0900

    Kanna Init

---
+++ Pods/Target Support Files/Pods-MyRSS-MyRSSUITests/Pods-MyRSS-MyRSSUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyRSS_MyRSSUITests : NSObject
+@end
+@implementation PodsDummy_Pods_MyRSS_MyRSSUITests
+@end
---
+++ Pods/Target Support Files/Pods-MyRSSTests/Pods-MyRSSTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyRSSTests : NSObject
+@end
+@implementation PodsDummy_Pods_MyRSSTests
+@end
