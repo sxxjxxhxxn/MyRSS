39045ea271b8ca41e24ae3480da43ab9e34ca5e5
Jaehooooooon <jaehoon572@naver.com>
2020-03-19 20:11:37 +0900

    Initialize

---
+++ MyRSS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyRSS
+//
+//  Created by 서재훈 on 2020/03/19.
+//  Copyright © 2020 서재훈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
